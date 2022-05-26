f6d2456b68b15a27bece98028ebd0048b75916eb
Anna Nikiforyak <a.nikiforyak@gmail.com>
2022-05-26 15:53:28 +0300

    added HW for lesson_4

---
+++ lesson_4/server.py
@@ -0,0 +1,89 @@
+"""Программа-сервер"""
+
+import socket
+import sys
+import json
+from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+from common.utils import get_message, send_message
+
+
+def process_client_message(message):
+    '''
+    Обработчик сообщений от клиентов, принимает словарь -
+    сообщение от клинта, проверяет корректность,
+    возвращает словарь-ответ для клиента
+
+    :param message:
+    :return:
+    '''
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    '''
+    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+    Сначала обрабатываем порт:
+    server.py -p 8079 -a 192.168.0.100
+    :return:
+    '''
+
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    # Затем загружаем какой адрес слушать
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+
+    except IndexError:
+        print(
+            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    # Готовим сокет
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    server_socket.listen(MAX_CONNECTIONS)
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        try:
+            message_from_cient = get_message(client_socket)
+            print(message_from_cient)
+            # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+            response = process_client_message(message_from_cient)
+            send_message(client_socket, response)
+            client_socket.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Принято некорретное сообщение от клиента.')
+            client_socket.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ lesson_4/unit_tests/test_server.py
@@ -0,0 +1,24 @@
+from lesson_4.server import process_client_message
+from lesson_4.common.variables import *
+import unittest
+
+
+class TestServer(unittest.TestCase):
+
+    ok_answer = {RESPONSE: 200}
+    err_answer = {RESPONSE: 400, ERROR: 'Bad Request'}
+
+    def test_no_action(self):
+        self.assertEqual(process_client_message({TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.err_answer)
+
+    def test_action_incorrect(self):
+        self.assertEqual(process_client_message({ACTION: 'incorrect', TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.err_answer)
+
+    def test_no_time(self):
+        self.assertEqual(process_client_message({ACTION: PRESENCE, USER: {ACCOUNT_NAME: 'Guest'}}), self.err_answer)
+
+    def test_not_guest(self):
+        self.assertEqual(process_client_message({ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'not a Guest'}}), self.err_answer)
+
+    def test_ok(self):
+        self.assertEqual(process_client_message({ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.ok_answer)
